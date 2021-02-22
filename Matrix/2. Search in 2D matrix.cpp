121a66f19016d13e2a0412c2e0e30b7b870ba2aa
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-02-22 11:33:30 +0530

    4 Approaches

--- Matrix/2. Search in 2D matrix.cpp
+++ Matrix/2. Search in 2D matrix.cpp
@@ -6,15 +6,15 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-       int i=0,j=matrix[0].size()-1;
+       int i=0,j=matrix[0].size()-1;                  // pointing to top-right element in matrix 
         while(i<matrix.size() && j>=0)
         {
             if(matrix[i][j]==target)
                 return true;
             else if(matrix[i][j]>target)
-                j--;
+                j--;                                  //going left
             else
-                i++;
+                i++;                                  //going down
         }
         return false;
     }
@@ -26,23 +26,25 @@ Approach 3: Copy all elements in a 1-D Array & then do binary search in that arr
 TC: O(Log(n*m))  AS: O(n*m)
 
 Approach 4: Imaginary Binary Search - Without auxillary array
+We are imagining a 1D Matrix in place of 2D as elements are sorted row wise and column wise and then we do binary search in that imaginary 1D Matrix  refer striver's video https://youtu.be/ZYpYur0znng?t=444
+    
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        if(!matrix.size()) return false;
+        if(!matrix.size()) return false;            //check for empty matrix
         
-        int m=matrix.size();
-        int n=matrix[0].size();
+        int m=matrix.size();                        //number of rows
+        int n=matrix[0].size();                    //number of columns
         
         int low=0;
-        int high=(n*m)-1;
+        int high=(n*m)-1;                
         
         while(low <= high)
         {
             int mid=low+(high-low)/2;
             
-            if(matrix[mid/n][mid%n]==target)
+            if(matrix[mid/n][mid%n]==target)       //trick for getting row and column number of 2D matrix from index of 1D Matrix
                 return true;
             else if(matrix[mid/n][mid%n]>target)
                 high=mid-1;
