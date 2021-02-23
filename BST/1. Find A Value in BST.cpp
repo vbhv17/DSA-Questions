a13dec7b3b87da6ed6140350b4a03af27e8587f3
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-02-23 23:21:03 +0530

    Search & Insert: Iterative & Recursive

--- BST/1. Find A Value in BST.cpp
+++ BST/1. Find A Value in BST.cpp
@@ -40,3 +40,62 @@ bool search( Node *root, int x )
 }
 
 TC: O(h)  AS:O(1)
+ 
+ 
+ Insertion In BST 
+ Recursive & Iterative
+ 
+ Recursive:
+Node *insert(Node *root, int x)
+{
+
+   if(root==NULL)
+      return new Node(x);
+
+   else if(root->key<x)
+      root->right=insert(root->right,x);
+
+   else if(root->key>x)
+      root->left=insert(root->left,x);
+   
+   return root;
+
+}
+TC: O(h)  AS:O(h)
+
+ 
+ Iterative:
+Node *insert(Node *root, int x)
+{
+
+   Node *temp = new Node(x);             //creating a new node
+   Node *parent = NULL;                  //maintining parent pointer for getting accces to leaf node (previous of curr) to which new node would be attached because curr will become NULL
+   Node *curr = root;
+   while (curr != NULL)
+   {
+      parent = curr;                     
+
+      if (cur->key > x)
+         curr = curr->left;
+
+      else if (curr->key < x)
+         curr = curr->right;
+
+      else
+         return root;
+
+   }
+
+   if (parent == NULL)
+      return temp;
+
+   if (parent->key > x)
+      parent->left = temp;
+
+   else
+      parent->right = temp;
+
+   return root;
+
+}
+TC: O(h)  AS:O(1)
