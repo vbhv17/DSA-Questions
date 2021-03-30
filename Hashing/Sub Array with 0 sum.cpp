d7ed6a237c8586f54a79e42accf436f1e4f28c39
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-03-31 00:04:20 +0530

    Using Prefix Sum

--- Hashing/Sub Array with 0 sum.cpp
+++ Hashing/Sub Array with 0 sum.cpp
@@ -19,3 +19,24 @@ bool subArrayExists(int arr[], int n)
 TC: O(nxn) AS:O(1) 
   
 Efficient Solution: 
+# Refer Notes
+  bool subArrayExists(int arr[], int n)
+  {
+    unordered_set<int> s;
+    int pre_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+      pre_sum += arr[i];
+      if (s.find(pre_sum) != s.end())
+        return true;
+
+      if (pre_sum == 0)
+        return true;
+
+      s.insert(pre_sum);
+
+    }
+    return false;
+  }
+
+TC:O(N)  AS:O(N)
