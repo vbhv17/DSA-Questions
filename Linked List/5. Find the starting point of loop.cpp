9e352769a05048b4da257a07ee06b83cc885aa96
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-04-20 22:40:27 +0530

    1 Approach

---
+++ Linked List/5. Find the starting point of loop.cpp
@@ -0,0 +1,44 @@
+Approach:
+# 1. If a loop is found, initialize a slow pointer to head, let fast pointer be at its position. 
+# 2. Move both slow and fast pointers one node at a time. 
+# 3. The point at which they meet is the start of the loop.
+
+
+
+Node* detectAndRemoveLoop(Node* head)
+{
+    // If list is empty or has only one node
+    // without loop
+    if (head == NULL || head->next == NULL)
+        return NULL;
+ 
+    Node *slow = head, *fast = head;
+ 
+    // Move slow and fast 1 and 2 steps
+    // ahead respectively.
+    slow = slow->next;
+    fast = fast->next->next;
+ 
+    // Search for loop using slow and
+    // fast pointers
+    while (fast && fast->next) {
+        if (slow == fast)
+            break;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+ 
+    // If loop does not exist
+    if (slow != fast)
+        return NULL;
+ 
+    // If loop exists. Start slow from
+    // head and fast from meeting point.
+    slow = head;
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+ 
+    return slow;
+}
