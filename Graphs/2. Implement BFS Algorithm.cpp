1381c119f33e19ae763e53435e43ba24eabb355d
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-05-23 13:11:09 +0530

    Update 2. Implement BFS Algorithm.cpp

--- Graphs/2. Implement BFS Algorithm.cpp
+++ Graphs/2. Implement BFS Algorithm.cpp
@@ -64,3 +64,42 @@ void BFSDis(vector<int> adj[], int v)   //function for checking for any disconne
 }
   
 TC: O(V+E)   AS:O(V)
+
+//Striver's Solution (Graph Playlist)   https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+    class Solution
+{
+public:
+  //Function to return Breadth First Traversal of given graph.
+  vector<int>bfsOfGraph(int V, vector<int> adj[])
+  {
+    vector<int> bfs;
+
+    vector<int> vis(V + 1, 0);
+    for (int i = 0; i < V; i++)
+    {
+      if (!vis[i])
+      {
+        queue<int> q;
+        q.push(i);
+        vis[i] = 1;
+
+        while (!q.empty())
+        {
+          int node = q.front();
+          q.pop();
+          bfs.push_back(node);
+
+          for (auto it : adj[node])
+          {
+            if (!vis[it])
+            {
+              q.push(it);
+              vis[it] = 1;
+            }
+          }
+        }
+      }
+    }
+    return bfs;
+  }
+};
