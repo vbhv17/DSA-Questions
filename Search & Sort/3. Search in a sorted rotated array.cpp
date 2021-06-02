8f90150e81db0709cd7bd70dc11bd9cc9e437d22
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-06-03 01:55:55 +0530

    Search in a sorted rotated array

---
+++ Search & Sort/3. Search in a sorted rotated array.cpp
@@ -0,0 +1,37 @@
+https://www.youtube.com/watch?v=oTfPJKGEHcc&ab_channel=TECHDOSE
+
+
+class Solution {
+public:
+  int search(vector<int>& nums, int target) {
+
+    int left = 0, right = nums.size() - 1;
+
+    while (left <= right)
+    {
+      int mid = (left + right) / 2;
+
+      if (nums[mid] == target)
+        return mid;
+
+      else if (nums[left] <= nums[mid])
+      {
+        if (target <= nums[mid] && target >= nums[left])
+          right = mid - 1;
+
+        else
+          left = mid + 1;
+      }
+
+      else
+      {
+        if (target >= nums[mid] && target <= nums[right])
+          left = mid + 1;
+
+        else
+          right = mid - 1;
+      }
+    }
+    return -1;
+  }
+};
