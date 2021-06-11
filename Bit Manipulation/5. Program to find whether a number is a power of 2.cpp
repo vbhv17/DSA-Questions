ff0ad1473c06c74b34c9ae7e6f2ec53d8dd6f881
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-06-11 12:41:50 +0530

    Check if N is power of two

---
+++ Bit Manipulation/5. Program to find whether a number is a power of 2.cpp
@@ -0,0 +1,26 @@
+If a number is a power of two , it will have exactly only one bit set in its binary representation
+
+class Solution {
+public:
+  // Function to check if given number n is a power of two.
+  bool isPowerofTwo(long long n) {
+
+    if (n == 0)             //Base Case
+      return false;
+
+    int count = 0;
+    while (n > 0)
+    {
+      n = n & (n - 1);    //Using Brian Kerningans Algorithm to count set bits
+      count++;
+    }
+
+    if (count > 1)
+      return false;
+    else
+      return true;
+  }
+};
+
+TC: O(No if set bits in N)   //Brian Kerninghans
+SC: O(1)
