d2a68405937c8a879546fb45465525c93fdfca77
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-06-23 01:03:22 +0530

    Minimum no of swaps required to sort the array

---
+++ Search & Sort/18. Minimum no of swaps required to sort the array.cpp
@@ -0,0 +1,37 @@
+Approach: We have an unsorted array, we store every element of this unsorted array with its index 
+          in a vector of pairs. Now we sort this vector, we get elements in sorted order.
+          No we try to convert this sorted vector to the original array.
+          We count the no of swaps we do, in order to do this
+           
+class Solution
+{
+public:
+  //Function to find the minimum number of swaps required to sort the array.
+  int minSwaps(vector<int>&nums)
+  {
+    int n = nums.size();
+    vector<pair<int, int>> v(n) ;
+
+    for (int i = 0; i < n; i++)
+      v[i] = {nums[i], i};
+
+    sort(v.begin(), v.end());
+
+    int c = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (i == v[i].second)
+        continue;
+      else {
+        c++;
+        swap(v[i], v[v[i].second]);
+        i--;
+      }
+
+    }
+    return c;
+  }
+};
+
+TC: O(NlogN)
+SC: O(N)
