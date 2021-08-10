50bb5eb2621ce9dba9ab5e23ed92b8ffb60d9406
vbhv17 <59169568+vbhv17@users.noreply.github.com>
2021-08-10 12:28:08 +0530

    Create 1. 0-1 Knapsack.cpp

---
+++ Dynamix Programming/1. 0-1 Knapsack.cpp
@@ -0,0 +1,16 @@
+Recursive Code:
+int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        //Base Condition
+        if(n==0 || W==0)
+          return 0;
+        
+        if(wt[n-1]<=W){
+            return max((val[n-1]+knapSack(W-wt[n-1], wt, val, n-1)), knapSack(W, wt, val, n-1));
+        }
+        
+        else
+          return knapSack(W, wt, val, n-1);
+          
+    }
+
